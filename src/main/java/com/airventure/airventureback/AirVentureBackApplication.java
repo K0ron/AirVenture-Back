aa394f5009dc6cc76a294cee0a22d9b0f2a3e283
Thomas Geslin <thomas.geslin31@gmail.com>
2024-01-18 11:11:09 +0100

    initial commit

---
+++ src/main/java/com/airventure/airventureback/AirVentureBackApplication.java
@@ -0,0 +1,13 @@
+package com.airventure.airventureback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirVentureBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirVentureBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airventure/airventureback/AirVentureBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airventure.airventureback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirVentureBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
