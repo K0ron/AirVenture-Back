242f88e787b81c5711a78e66d28e439a8d5ecab0
Thomas Geslin <thomas.geslin31@gmail.com>
2024-03-22 09:34:07 +0100

    feat: adding of the picture upload

--- src/main/java/com/airventure/airventureback/upload/application/FileUploadController.java
+++ src/main/java/com/airventure/airventureback/upload/application/FileUploadController.java
@@ -1,14 +1,12 @@
 package com.airventure.airventureback.upload.application;
 
 import com.airventure.airventureback.upload.domain.service.IStorageService;
-import com.airventure.airventureback.upload.infrastructure.exception.StorageFileNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
 
 @Controller
 public class FileUploadController {
@@ -22,25 +20,8 @@ public class FileUploadController {
 
     @PostMapping("/upload")
     public ResponseEntity<MultipartFile> handleFileUpload(@RequestParam("file") MultipartFile file) {
+        System.out.println(file);
         storageService.store(file);
-        return ResponseEntity.status(201).build();
-    }
-
-    @GetMapping("/files")
-    @ResponseBody
-    public ResponseEntity<Resource> serveFile() {
-
-        Resource file = storageService.loadAsResource("690jxXKCUtWmbsWnN4gibBUYR20ahsrc3P5v0jMIh4FCexCaSF-600x600.jpg");
-
-        if (file == null)
-            return ResponseEntity.notFound().build();
-
-        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
-                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
-    }
-
-    @ExceptionHandler(StorageFileNotFoundException.class)
-    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
-        return ResponseEntity.notFound().build();
+        return ResponseEntity.status(200).build();
     }
 }
