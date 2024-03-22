84601669e3eb05594fe14cd9fe693b3fa4be6c0f
Thomas Geslin <thomas.geslin31@gmail.com>
2024-03-22 09:34:07 +0100

    feat: adding of the picture upload

--- src/main/java/com/airventure/airventureback/upload/domain/service/IStorageService.java
+++ src/main/java/com/airventure/airventureback/upload/domain/service/IStorageService.java
@@ -1,4 +1,23 @@
 package com.airventure.airventureback.upload.domain.service;
 
-public interface IStroageService {
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public interface IStorageService {
+    void init();
+
+    void store(MultipartFile file);
+
+    Stream<Path> loadAll();
+
+    Path load(String filename);
+
+    Resource loadAsResource(String filename);
+
+    void deleteAll();
+
+    String cleanFileName(String originalFileName);
 }
